49cc55d5bd95161ad6699c0b058bbeb3c9d81648
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2023-05-03 23:58:04 +0900

    리액트 들어가기 전 SPA 연습

--- React/spa/app.js
+++ React/spa/app.js
@@ -1,6 +1,6 @@
 const container = document.querySelector("#root");
 
-// 환경변수(대문자): 깃허브 등에 올릴 때 공개되지 않게 하기 위해
+// 환경변수
 const NEWSLIST_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/$id.json";
 
@@ -13,8 +13,7 @@ const store = {
 // api 데이터 받아오는 함수
 async function getData(url) {
   try {
-    const response = await fetch(url); // 비동기적으로 실행되므로 데이터가 들어올 때까지 기다려야된다(await)
-
+    const response = await fetch(url);
     if (!response.ok) {
       throw new Error("네트워크에 문제가 있습니다.");
     }
@@ -31,42 +30,46 @@ async function newsFeed() {
   const newsList = [];
 
   newsList.push("<ul>");
+
   for (
     let i = (store.currentPage - 1) * store.datasPerPage;
     i < store.currentPage * store.datasPerPage;
     i++
   ) {
     newsList.push(`
-      <li>
-        <a href="#/detail/${newsFeed[i].id}">${newsFeed[i].title}(${newsFeed[i].comments_count})</a>
-      </li>
-    `);
+            <li>
+                <a href="#/detail/${newsFeed[i].id}">${newsFeed[i].title}(${newsFeed[i].comments_count})</a>
+            </li>
+        `);
   }
+
   newsList.push("</ul>");
 
-  let pageList = ``;
+  let pageList = "";
   for (let i = 0; i < totalPages; i++) {
     pageList += `<li><a href="#/page/${i + 1}">${i + 1}</a></li>`;
   }
+
   newsList.push(
     `<nav>
-      <ul>
-        ${pageList}
-      </ul>
-    </nav>`
+            <ul>
+                ${pageList}
+            </ul>
+        </nav>`
   );
 
   container.innerHTML = newsList.join("");
 }
 
+// 뉴스 디테일 페이지 구성 함수
 async function newsDetail() {
-  const id = location.hash.substring(9); // hash: #(Hash)다음 주소
+  const id = location.hash.substring(9);
   const newsContent = await getData(CONTENT_URL.replace("$id", id));
 
   container.innerHTML = `
-    <h1>${newsContent.title}</h1>
-    <div><a href="#/page/${store.currentPage}">목록으로 돌아가기</a></div>
-  `;
+        <h1>${newsContent.title}</h1>
+        <div><a href="#/page/${store.currentPage}">목록으로 돌아가기</a></div>
+    `;
 }
 
 // 라우터: 화면을 중계하는 함수
